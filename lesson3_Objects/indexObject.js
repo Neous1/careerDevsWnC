feb21ab699319347b6f50e28bee21c6fa0060ed4
Neous1 <Neous1@users.noreply.github.com>
2017-07-09 12:40:04 -0400

    should store array in an object

--- lesson3_Objects/indexObject.js
+++ lesson3_Objects/indexObject.js
@@ -8,11 +8,29 @@ var myComputer = {
 console.log(myComputer);
 console.log(myComputer.operatingSystem);
 
+/* /////////////
+Lesson 3 Objects*
+///////////*/
+
 var yvon  = {
     name: "Yvon",
     sayName : function(){
-        console.log(this)// keyword this refers to the entire function
+        console.log(this.name)// keyword this refers to the entire object
     }
 }
 
 yvon.sayName()
+
+
+// 1. it should store the todos array on an object
+
+var todoList = {
+    todos:["item1", "item2" , "item3", "item4"]
+}
+
+console.log(todoList);
+
+// 2. it should have a displayTodos method 
+// 3. it should have a addTodo method 
+// 4. it should have a changeTodo method 
+// 5. it should have a deleteTodo method 
