7bfbc4a21d077dd840eeba8114a0b3464d553e27
Neous1 <Neous1@users.noreply.github.com>
2017-07-09 18:09:11 -0400

    add an object to organize the process of the Todo list

--- lesson3_Objects/indexObject.js
+++ lesson3_Objects/indexObject.js
@@ -25,12 +25,37 @@ yvon.sayName()
 // 1. it should store the todos array on an object
 
 var todoList = {
-    todos:["item1", "item2" , "item3", "item4"]
-}
+    todos:["item1", "item2" , "item3", "item4"],
+    // 2. it should have a displayTodos method 
+    displayTodos: function(){
+        console.log("My todos:", this.todos)
+    },
+    // 3. it should have a addTodo method 
+    addTodo: function(todo){
+        this.todos.push(todo);// the "this" keyword infers a push tothe todos array above
+        this.displayTodos()// the "this" keyword calls the displayTodos function above
+    },
+    // 4. it should have a changeTodo method 
+    changeTodo: function(position, newValue){
+        this.todos[position] = newValue;
+        this.displayTodos();
+    },
+    // 5. it should have a deleteTodo method 
+    deleteTodo: function(position){
+        this.todos.splice(position,1);
+        this.displayTodos();
+    }
+};
 
-console.log(todoList);
+// console.log(todoList);
+todoList.displayTodos()
+todoList.addTodo('plunker')
+todoList.changeTodo(4, 'changed plunker')
+todoList.deleteTodo(4)
 
-// 2. it should have a displayTodos method 
-// 3. it should have a addTodo method 
-// 4. it should have a changeTodo method 
-// 5. it should have a deleteTodo method 
+
+
+
+
+
+
