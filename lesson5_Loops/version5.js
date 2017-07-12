232c732785b46bf62aac0d23d819fc7125c4dce4
Neous1 <Neous1@users.noreply.github.com>
2017-07-12 13:40:20 -0400

    loop thru todolist and display todos

--- lesson5_Loops/version5.js
+++ lesson5_Loops/version5.js
@@ -7,3 +7,47 @@ for (var i=0; i < testArray.length; i++){
     console.log(testArray[i]);
 
 }
+
+
+
+// displayTodos shoud tell you if .todos is empty
+// displayTodos shoud show .completed
+
+
+var todoList = {
+    todos:[],
+    // displayTodos should show .todoText
+    displayTodos: function(){
+        console.log("My todos:", this.todos)
+        for (var i= 0; i <this.todos.length; i++){
+            console.log("displayed Todos:",this.todos[i].todoText)
+        }
+    },
+    // todoList.addTodo should add objects
+    addTodo: function(todoText){
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+        this.displayTodos()
+    },
+    // todoList.changeTodo should change the todoText property
+    changeTodo: function(position, todoText){
+        this.todos[position].todoText = todoText;
+        this.displayTodos();
+    },
+    // 5. it should have a deleteTodo method 
+    deleteTodo: function(position){
+        this.todos.splice(position,1);
+        this.displayTodos();
+    },
+    // todoList.toggleCompleted should change the completed  property
+    toggleCompleted: function(position){
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    }
+};
+todoList.displayTodos();
+todoList.addTodo('first');
+todoList.addTodo('second');
