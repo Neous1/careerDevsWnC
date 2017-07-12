44fca9d192f058a607ab0c3f34960b5e4f0a1788
Neous1 <Neous1@users.noreply.github.com>
2017-07-12 19:40:52 -0400

    add index.html file

---
+++ lesson7_HTML/version7.js
@@ -0,0 +1,81 @@
+var todoList = {
+    todos:[],
+    
+    displayTodos: function(){
+// displayTodos shoud tell you if .todos is empty        
+        if(this.todos.length === 0){
+            console.log("Your todo list is empty!");
+        }
+        else{ // displayTodos should show .todoText
+            console.log("My todos:")
+            for (var i= 0; i <this.todos.length; i++){
+                // console.log("displayed Todos:",this.todos[i].todoText)
+                // displayTodos shoud show .completed
+                if(this.todos[i].completed === true){
+                    console.log("(x)",this.todos[i].todoText)
+                }
+                else{
+                    console.log("( )",this.todos[i].todoText)
+                }
+            }
+        }
+    },
+    // todoList.addTodo should add objects
+    addTodo: function(todoText){
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+        this.displayTodos()
+    },
+    // todoList.changeTodo should change the todoText property
+    changeTodo: function(position, todoText){
+        this.todos[position].todoText = todoText;
+        this.displayTodos();
+    },
+    // 5. it should have a deleteTodo method 
+    deleteTodo: function(position){
+        this.todos.splice(position,1);
+        this.displayTodos();
+    },
+    // todoList.toggleCompleted should change the completed  property
+    toggleCompleted: function(position){
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    },
+    // .toggleAll: if everything's true , make everything false.
+    toggleAll: function(){
+        var totaltodos = this.todos.length;
+        var completedTodos = 0;
+        //Get number of completed todos.
+        for(var i= 0; i < totaltodos; i++){
+            if (this.todos[i].completed=== true){
+                completedTodos++;
+            }
+        }
+        //Case 1: If everything is true , make everything false.
+        if(completedTodos === totaltodos){
+            for (var i=0; i<totaltodos; i++){
+                this.todos[i].completed = false
+            }
+        }
+        // .toggelAll: Otherwise, make everything true.
+        else{
+             for (var i=0; i<totaltodos; i++){
+             this.todos[i].completed = true;
+            }
+        }
+        this.displayTodos();
+    }
+
+};
+//1. We want to get access to the display todos button.
+
+var displayTodosButton = document.getElementById('displayTodosButton')
+
+console.log(displayTodosButton);
+
+// There should be a "Display todos" and a "Toggle All" button in teh App
+// Clicking Display todos should run todoList.DisplayTodos.
+// Clicking "Toggle All" should run todoList.toggleAll.
