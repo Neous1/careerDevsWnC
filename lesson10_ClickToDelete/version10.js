2666ee4b7df7600c7ebc9aec6954343c144b6aa5
Neous1 <Neous1@users.noreply.github.com>
2017-07-14 04:06:56 -0400

    Add a createDeleteButton to view

---
+++ lesson10_ClickToDelete/version10.js
@@ -0,0 +1,110 @@
+var todoList = {
+    todos:[],
+    addTodo: function(todoText){
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+        this.displayTodos()
+    },
+    // todoList.changeTodo should change the todoText property
+    changeTodo: function(position, todoText){
+        this.todos[position].todoText = todoText;
+    },
+    // 5. it should have a deleteTodo method 
+    deleteTodo: function(position){
+        this.todos.splice(position,1);
+    },
+    // todoList.toggleCompleted should change the completed  property
+    toggleCompleted: function(position){
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+    },
+    // .toggleAll: if everything's true , make everything false.
+    toggleAll: function(){
+        var totaltodos = this.todos.length;
+        var completedTodos = 0;
+        //Get number of completed todos.
+        for(var i= 0; i < totaltodos; i++){
+            if (this.todos[i].completed=== true){
+                completedTodos++;
+            }
+        }
+        //Case 1: If everything is true , make everything false.
+        if(completedTodos === totaltodos){
+            for (var i=0; i<totaltodos; i++){
+                this.todos[i].completed = false
+            }
+        }
+        // .toggelAll: Otherwise, make everything true.
+        else{
+             for (var i=0; i<totaltodos; i++){
+             this.todos[i].completed = true;
+            }
+        }
+        this.displayTodos();
+    }
+
+};
+// handlers will handle different events
+var handlers = {
+    addTodo: function(){
+        var addTodoTextInput = document.getElementById("addTodoTextInput");
+        todoList.addTodo(addTodoTextInput.value)
+        addTodoTextInput.value = " ";
+        view.displayTodos();
+        
+    },
+    changeTodo: function(){
+        var changeTodoPositionInput = document.getElementById("changeTodoPositionInput");
+        var changeTodoTextInput = document.getElementById("changeTodoTextInput");
+        todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
+        changeTodoPositionInput.value = "";
+        changeTodoTextInput.value = "";
+        view.displayTodos();
+    },
+    deleteTodo: function(){
+        var deleteTodoPositionInput = document.getElementById("deleteTodoPositionInput");
+        todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
+        deleteTodoPositionInput.value = "";
+        view.displayTodos();
+    }, 
+    toggleCompleted: function(){
+        var toggleCompletedPositionInput = document.getElementById("toggleCompletedPositionInput");
+        todoList.toggleCompleted(toggleCompletedPositionInput.valueAsNumber);
+        toggleCompletedPositionInput.value = "";
+        view.displayTodos();
+    },
+    toggelAll: function(){
+        todoList.toggleAll();
+        view.displayTodos();
+    }
+};
+
+var view = {
+    displayTodos: function(){
+        var todosUl = document.querySelector('ul');
+        todosUl.innerHTML="";
+        for(var i = 0; i< todoList.todos.length; i++){
+            var todoLi = document.createElement("li");
+            var todo = todoList.todos[i];
+            var todoTextWithCompletion = "";
+
+            if(todo.completed === true){
+                todoTextWithCompletion = "(x) " + todo.todoText;
+            }
+            else {
+                todoTextWithCompletion = "( ) " + todo.todoText;
+            }
+            todoLi.textContent = todoTextWithCompletion;
+            todosUl.appendChild(todoLi);
+        }
+
+    },
+    createDeleteButton: function(){
+        var deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete";
+        deleteButton.className = "deleteButton";
+        return deleteButton;
+    }
+}
